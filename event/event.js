c56fcad3b42deb1e5697a8ac55953f66aaa135dc
ayubparvej <ayub.pervej1@gmail.com>
2024-04-04 17:01:26 +0600

    events

--- event/event.js
+++ event/event.js
@@ -16,3 +16,27 @@ const btnInpt = document.getElementById('show-text').addEventListener("click", f
 
 
 });
+
+const textValue = document.getElementById('input-value').addEventListener('keyup', function (event) {
+
+    const textInput = document.getElementById('input-value');
+    const text = event.target.value;
+
+    const btnDelete = document.getElementById("btn-delete");
+
+    if (text === 'delete' || text === 'Delete') {
+        //btnDelete.disabled = false;
+        btnDelete.removeAttribute("disabled");
+
+    } else {
+        //btnDelete.disabled = true;
+        btnDelete.setAttribute("disabled", "true");
+    }
+})
+
+const deleteBtn = document.getElementById('btn-delete').addEventListener('click', function () {
+    const textHide = document.getElementById('hide-text');
+    textHide.style.display = 'none';
+
+
+})
